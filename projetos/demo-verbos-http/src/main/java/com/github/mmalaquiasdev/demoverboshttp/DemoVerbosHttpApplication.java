fdc2ef20c40f16e7437f618df1defa57e2c9292f
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-05 07:56:38 -0300

    Subindo estrutura da demo de verbos http

---
+++ projetos/demo-verbos-http/src/main/java/com/github/mmalaquiasdev/demoverboshttp/DemoVerbosHttpApplication.java
@@ -0,0 +1,12 @@
+package com.github.mmalaquiasdev.demoverboshttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVerbosHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVerbosHttpApplication.class, args);
+	}
+}
