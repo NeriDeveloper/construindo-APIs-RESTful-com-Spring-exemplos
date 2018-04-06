a35fbc86f3126ffc4959a1a4f7370ebc3c4fd742
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-06 01:02:27 -0300

    Adicionando anotação no metodo para responder as chamadas de acordo com o status que eu quero

--- projetos/demo-status-code/src/main/java/com/github/mmalaquiasdev/demoverboshttp/api/UsuarioApi.java
+++ projetos/demo-status-code/src/main/java/com/github/mmalaquiasdev/demoverboshttp/api/UsuarioApi.java
@@ -3,6 +3,7 @@ package com.github.mmalaquiasdev.demoverboshttp.api;
 import com.github.mmalaquiasdev.demoverboshttp.dominio.Usuario;
 import com.github.mmalaquiasdev.demoverboshttp.servico.UsuarioServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,26 +14,31 @@ public class UsuarioApi {
     private UsuarioServico servico;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<Usuario> pesquisar(){
        return servico.pesquisar();
     }
 
     @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Usuario pesquisarPorId(@PathVariable Long id) {
         return servico.pesquisarPorId(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Usuario salvar(@RequestBody Usuario usuario) {
         return servico.salvar(usuario);
     }
 
     @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Usuario atualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
         return servico.atualizar(usuario);
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long id) {
         servico.deletar(id);
     }
