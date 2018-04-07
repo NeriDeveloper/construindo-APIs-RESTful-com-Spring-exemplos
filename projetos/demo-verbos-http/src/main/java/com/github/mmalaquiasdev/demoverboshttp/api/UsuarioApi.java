624288cb080b36029e2e7d450843a9901d7f04fe
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-04-07 09:17:54 -0300

    Removendo demo dos media types pq vou subir uma atualizada. Tb estou removendo imports não utilizados na demo dos verbos

--- projetos/demo-verbos-http/src/main/java/com/github/mmalaquiasdev/demoverboshttp/api/UsuarioApi.java
+++ projetos/demo-verbos-http/src/main/java/com/github/mmalaquiasdev/demoverboshttp/api/UsuarioApi.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
